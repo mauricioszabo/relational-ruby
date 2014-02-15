f29f7005b5e64c5ada78e75ef1791be130e21070
Maurício Szabo <mauricio.szabo@gmail.com>
2014-02-15 09:37:25 -0200

    Added a NOT clause for IN.

--- lib/relational/comparissions/in.rb
+++ lib/relational/comparissions/in.rb
@@ -3,13 +3,17 @@ require_relative '../partial'
 module Relational
   module Comparissions
     class In < Partial
-      def initialize(attribute, items)
-        @attribute, @items = attribute, items
+      def initialize(attribute, items, negate=false)
+        @attribute, @items, @negate = attribute, items, negate
       end
 
       lazy :partial do
         partial = @attribute.partial
-        PartialStatement.new("#{partial.query} IN (?)", partial.attributes + [@items])
+        if @negate
+          PartialStatement.new("#{partial.query} NOT IN (?)", partial.attributes + [@items])
+        else
+          PartialStatement.new("#{partial.query} IN (?)", partial.attributes + [@items])
+        end
       end
     end
   end
