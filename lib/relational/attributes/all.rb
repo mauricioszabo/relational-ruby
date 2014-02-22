d14c6aa6c7737053358207e6b084abcddbc23cbf
Maurício Szabo <mauricio.szabo@gmail.com>
2014-02-22 20:35:12 -0300

    Corrected All attribute.

--- lib/relational/attributes/all.rb
+++ lib/relational/attributes/all.rb
@@ -3,12 +3,16 @@ require_relative 'attribute_like'
 
 module Relational
   module Attributes
-    class AllClass < Modifiable
+    class AllClass < AttributeLike
+      extend Adapter::FunctionDefinition
       include Singleton
 
       def partial
         PartialStatement.new('*', [])
       end
+
+      define_function1 :count
+      define_function1 :count_distinct, "COUNT(DISTINCT $1)"
     end
 
     All = AllClass.instance
