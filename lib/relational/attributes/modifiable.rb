7d4ada06919ab22e871cde1fc42f63ad3b03a0ae
Maurício Szabo <mauricio.szabo@gmail.com>
2014-02-22 20:34:52 -0300

    Corrected Modifiable.

--- lib/relational/attributes/modifiable.rb
+++ lib/relational/attributes/modifiable.rb
@@ -1,29 +1,14 @@
 require_relative 'attribute_like'
 require_relative 'none'
-require_relative '../adapter'
+require_relative '../adapter/function_definition'
 
 module Relational
   module Attributes
     class Modifiable < AttributeLike
+      extend Adapter::FunctionDefinition
+
       def nil?
         self == nil
-      end
-
-      def self.define_function(function, string = "#{function.to_s.upcase}($1)")
-        Relational::Adapter.define_function function, all: ->(this) {
-          partial = this.partial
-          query = string.sub('$1', partial.query)
-          [query, partial.attributes]
-        }
-      end
-
-      def self.define_function2(function, string)
-        Relational::Adapter.define_function function, all: ->(this, operand) {
-          partial = this.partial
-          operand_p = Relational::Partial.wrap(operand).partial
-          query = string.sub('$1', partial.query).sub('$2', operand_p.query)
-          [query, partial.attributes + operand_p.attributes]
-        }
       end
 
       in_clause = ->(this, operand) do
@@ -35,7 +20,7 @@ module Relational
         )
       end
 
-      Relational::Adapter.define_custom_method :in?, all: ->(params) {
+      define_custom_method :in?, all: ->(params) {
         Relational::Comparissions::In.new(self, params)
       }, oracle: ->(params) {
         ins = params.each_slice(1000).map do |slice|
@@ -44,7 +29,7 @@ module Relational
         Relational::Comparissions::Or.new(ins)
       }
 
-      Relational::Adapter.define_custom_method :not_in?, all: ->(params) {
+      define_custom_method :not_in?, all: ->(params) {
         Relational::Comparissions::In.new(self, params, true)
       }, oracle: ->(params) {
         ins = params.each_slice(1000).map do |slice|
@@ -72,28 +57,28 @@ module Relational
       define_function2 :!~, '$1 NOT LIKE $2'
       define_function2 :not_like, '$1 NOT LIKE $2'
 
-      define_function :null?, "$1 IS NULL"
-      define_function :not_null?, "$1 IS NOT NULL"
-      define_function :!, "NOT($1)"
+      define_function1 :null?, "$1 IS NULL"
+      define_function1 :not_null?, "$1 IS NOT NULL"
+      define_function1 :!, "NOT($1)"
 
-      Relational::Adapter.define_custom_method :|, all: ->(*items) {
+      define_custom_method :|, all: ->(*items) {
         Relational::Comparissions::Or.new([self, *items])
       }
 
-      Relational::Adapter.define_custom_method :&, all: ->(*items) {
+      define_custom_method :&, all: ->(*items) {
         Relational::Comparissions::And.new([self, *items])
       }
 
-      define_function :sum
-      define_function :avg
-      define_function :max
-      define_function :min
-      define_function :count
-      define_function :count_distinct, "COUNT(DISTINCT $1)"
+      define_function1 :sum
+      define_function1 :avg
+      define_function1 :max
+      define_function1 :min
+      define_function1 :count
+      define_function1 :count_distinct, "COUNT(DISTINCT $1)"
 
-      define_function :length
-      define_function :upper
-      define_function :lower
+      define_function1 :length
+      define_function1 :upper
+      define_function1 :lower
     end
   end
 end
